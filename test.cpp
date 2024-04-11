fc030f322b9345c4b8352fa8d1ab4409f2904a10
davidesavona <davidesavona14@gmail.com>
2024-04-11 11:44:07 +0200

    working trigger

--- test.cpp
+++ test.cpp
@@ -6,80 +6,139 @@
 #include "motorDriver.h"
 
 
-int main()
-{
-    
+//#include <QCoreApplication>
+#include <signal.h>
+#include <wiringPi.h>
 
-    std::string  current;
-    int dc;
+#include <thread>
+#include <pthread.h>
+#include <numeric>
+#include <iostream>
+using namespace std;
 
-    motorDriver::InitMotor("/dev/ttyACM0", 115200, serial::Timeout::simpleTimeout(3000));
-    //
-    while(1){
-    motorDriver::startMotor();
+bool running = 1;
+bool oldstate=0;
+long int TIME=0;
+long int OLD_TIME=-1;
+int elapsed;
+double w;
+double seconds;
+double cfr = 2.096; // m
+double km = 0;
+double m = 0;
+bool firstPulse=0;
+double w_vect[5];
+double w_mean;
+double v;
+int i=0;
+int counter=0;
 
-    std::cout<<"ON"<<std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    motorDriver::stopMotor();
-    std::cout<<"OFF"<<std::endl;
+
+
+#define DEFAULT_LOOP_TIME_NS 1000000L
+#define TH1_PERIOD 5
+#define TH2_PERIOD 20
+
+timespec addition(timespec a, timespec b) {
+    timespec r;
+
+    if(a.tv_nsec + b.tv_nsec <= 999999999) {
+        r.tv_nsec = a.tv_nsec + b.tv_nsec;
+        r.tv_sec = a.tv_sec + b.tv_sec;
+    }
+    else {
+        int c = (a.tv_nsec + b.tv_nsec)/1000000000;
+        r.tv_nsec = a.tv_nsec + b.tv_nsec - 1000000000*c;
+        r.tv_sec = a.tv_sec + b.tv_sec + c;
     }
 
-    
-    //*************************************** Current reading*************************************
-    
-    // while(1){
-    // current = motorDriver::getCurrent();
-    // //std::cout<<"Current: "<<current<<std::endl;
-    // char arr[current.length() + 1]; 
-    // char* arrEnd;
+    return r;
+}
 
-	// strcpy(arr, current.c_str()); 
-    // if(current.empty()) std::cout<<"error"<<std::endl;
 
-    // //std::cout  << std::strtof(arr,&arrEnd) << " A" << std::endl;
-    // std::cout <<"Current:"<< std::stof(current) << " A" << std::endl;
-    // std::this_thread::sleep_for(std::chrono::milliseconds(20));
-    // }
+void *threadFunction(void* a) {
 
-    //**************************************************************************************************************
-    
-    
-    // for(int i = 0; i < 5; i++){
+    //signal(SIGINT, myInterruptHandler);
 
-    //     if(i%5 == 0){
+    //readfromSM();
 
-    //         dc = 200;
+    struct timespec t_now;
+    struct timespec t_next;
+    struct timespec t_period;
+    struct timespec t_wait;
+    struct timespec pause_duration;
 
-    //     }
-    //     else{ 
+    //unsigned long int t_overflow = 0;   // measure the overflowed time for each cycle
+    unsigned long int loop_count = 0;
 
-    //         dc = 50;
-    //     }
+    // t_period defines duration of one "running" cycle (1 ms in this case)
+    t_period.tv_sec = 0;
+    t_period.tv_nsec = DEFAULT_LOOP_TIME_NS;
 
-    //     count = motor::setDutyCycle(dc, 0);
-    //     total += count;
+    // pause_duration defines duration of pause considered when a buttton is pressed (1 s in this case)
+
+    clock_gettime( CLOCK_MONOTONIC, &t_next);
+
+    // Save into variables the first input variables from GUI
+    // TODO: save user variables
+
+
+   while(running) {
+
+       // chartclass.readButton();
+        t_next = addition(t_next, t_period); // update t_next (needed for usleep at the end)clock_gettime ( CLOCK_MONOTONIC, &t_now);
+
+        if(loop_count%1 == 0) {
         
-    // }
+        }
 
-    // std::cout << "Score: " << (total/5)*100 << "%" << std::endl;
+        if(loop_count%500==0){
+            if(oldstate) {
+            motorDriver::startMotor(); 
+            cout<<"start\n";
+            oldstate=!oldstate;
+            }else{
+            motorDriver::stopMotor();
+            cout<<"stop\n";
+            oldstate=!oldstate;
+            }
+            // cout<<"current: "<<motorDriver::getCurrent()<<endl;
+        //  motorDriver::startMotor();
+        }
 
-    // motor::stopMotor();
 
-    // for(int i = 10; i <= 50; i++){
 
-    //     if(i%5 == 0){
 
-    //         motor::setDutyCycle(i, 0);
-    //         //current = motor::getCurrent();
-    //         std::cout << current << " A" << std::endl;
-    //     }
+        loop_count++;
 
-    // }
+        clock_nanosleep ( CLOCK_MONOTONIC, TIMER_ABSTIME, &t_next, nullptr ); // waits until time t_next is reached
 
-   
-    //motorDriver::stopMotor();
-
-    return 0;
+        // print t_now (just to check "running" cycle repeats with desired frequency)
+        clock_gettime ( CLOCK_MONOTONIC, &t_now);
+        //cout << "\nT running = " << t_now.tv_sec*1000 + t_now.tv_nsec/1000000 << "\n";
+    }
 
 }
+
+
+int main (void)
+{
+ 
+    motorDriver::InitMotor("/dev/ttyACM0", 115200, serial::Timeout::simpleTimeout(3000));
+    //cout<<"current: "<<motorDriver::getCurrent()<<endl;
+
+//    while (1) {
+
+//}
+    pthread_t thread;
+    pthread_create(&thread, NULL, threadFunction, nullptr);
+    pthread_join(thread, NULL);
+
+
+
+return 0;
+
+}
+
+
 
